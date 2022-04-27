c9f522a4479579582fa4c7c9104dba082718d81e
Tunji545 <msknolatunji@gmail.com>
2022-04-27 04:47:30 +0100

    add shared module

--- src/app/departments/department.module.ts
+++ src/app/departments/department.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DepartmentListComponent } from './department-list.component';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { DepartmentDetailComponent } from './department-detail.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [DepartmentListComponent],
   imports: [
     CommonModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forChild([
       { path: 'lecturers', component: DepartmentListComponent },
       { path: 'lecturers/:id', component: DepartmentDetailComponent },
     ]),
+    SharedModule,
   ],
 })
 export class DepartmentModule {}
